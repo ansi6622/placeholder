96ffe3c0ab5b95631812c2281ec38b62aee68617
Anthony Simpson <ansi6622@colorado.edu>
2016-05-12 15:19:21 -0600

    added some server side validation for the jwt tokens that get created and passed around

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,10 +5,25 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 /* GET home page. */
 router.get('/me', function(req, res, next) {
-
 console.log(req.headers.authorization);
-
+if(req.headers.authorization){
+  const token = req.headers.authorization.split(' ')[1];
+  const payload = jwt.verify(token, process.env.JWT_SECRET);
+  knex('users').where({id: payload.id}).first().then(function(user){
+    if(user){
+      res.json({id: user.id, name: user.name})
+    }
+    else {
+      res.status(403).json({
+        error: "Invalid Id"
+      })
+    }
+  })
+} else {
   res.status(403).json({ error: 'No Token' });
+
+}
+
 });
 
 
